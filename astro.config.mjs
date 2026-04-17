6809f5dfdb9b63abced9ac2aa4d99774b797fc53
Duyên Ng <huongduyen.work@gmail.com>
2026-04-17 14:39:58 +0700

    ci~cd+content: create Cloudflare wrangler + update SEO tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-// @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+/** @var {string} GITHUB_BASE = '/arttooldatabase/' */
+console.log("📋•ENV: GITHUB_BASE=", process.env.GITHUB_BASE);
+
 // https://astro.build/config
 export default defineConfig({
-  base: '/arttooldatabase/',
+  base: process.env.GITHUB_BASE || '/',
   vite: {
     plugins: [tailwindcss()],
   },
